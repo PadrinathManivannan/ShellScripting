459f1ef5876a7adc80c26e86aa5c40cef672d3ec
PadrinathManivannan <165633729+PadrinathManivannan@users.noreply.github.com>
2024-04-30 16:33:40 +0530

    read_file.sh

---
+++ read_file.sh
@@ -0,0 +1,19 @@
+echo "Enter your Filename: \c"
+read fname
+if [-z "$fname"]
+then 
+  exit
+fi
+
+terminal = 'tty'
+
+exec < fname
+
+count = 1
+
+while read line
+do
+echo $count.$line
+count = 'expr $count +1'
+done
+exec < $terminal
